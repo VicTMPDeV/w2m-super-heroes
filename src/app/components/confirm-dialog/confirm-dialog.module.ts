b5481ffcee94babcfbaaed487c562d94438e7a01
Victor_Morales_Perez <vmorales@atsistemas.com>
2022-10-04 16:34:56 +0200

    remove mat-divider from confirm-dialog

--- src/app/components/confirm-dialog/confirm-dialog.module.ts
+++ src/app/components/confirm-dialog/confirm-dialog.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
 
 import { ConfirmDialogComponent } from '@components/confirm-dialog/confirm-dialog.component';
 
@@ -16,7 +15,6 @@ import { ConfirmDialogComponent } from '@components/confirm-dialog/confirm-dialo
     CommonModule,
     FlexLayoutModule,
     MatDialogModule,
-    MatDividerModule,
     MatButtonModule
   ],
   exports: [
